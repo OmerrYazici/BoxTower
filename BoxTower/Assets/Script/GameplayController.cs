c6424c8b47924b214f384ef61c6bf288edda3249
OmerrYazici <88666841+OmerrYazici@users.noreply.github.com>
2022-03-23 22:23:31 +0300

    Game

---
+++ BoxTower/Assets/Script/GameplayController.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameplayController : MonoBehaviour
+{
+    public static GameplayController instance;
+
+    public BoxSpawner box_Spawner;
+
+    [HideInInspector]
+    public BoxScript currentbox;
+
+    public Kam cameraScript;
+    private int moveCount;
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        box_Spawner.SpawnBox(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DetectInput();
+    }
+
+     void DetectInput()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            currentbox.DropBox();
+        }
+    }
+     public void SpawnNewBox()
+    {
+        Invoke("NewBox", 2f);
+
+    }
+
+    void NewBox()
+    {
+        box_Spawner.SpawnBox();
+    }
+    public void MoveCamera()
+    {
+        moveCount++;
+        if (moveCount == 3)
+        {
+            moveCount = 0;
+            cameraScript.targetPos.y += 2f;
+        }
+    }
+    public void RestartGame() {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(
+        UnityEngine.SceneManagement.SceneManager.GetActiveScene().name);
+    }
+}
---
+++ BoxTower/Assets/Script/Kam.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kam: MonoBehaviour
+{
+    [HideInInspector]
+    public Vector3 targetPos;
+
+    private float smoothMove = 1f;
+
+    private void Start()
+    {
+        targetPos = transform.position;
+    }
+    private void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, targetPos, smoothMove * Time.deltaTime);
+    }
+}
